a164ce5dae34dfb56fdf25a07bb9152edbff8f95
Antarctica Citizen <75155964+LeeJeongWoon-Workout@users.noreply.github.com>
2022-02-24 15:37:27 +0900

    Add files via upload

---
+++ Ensemble_DDPG/Network.py
@@ -0,0 +1,38 @@
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+
+
+class MuNet(nn.Module):
+    def __init__(self):
+        super(MuNet,self).__init__()
+        self.fc1=nn.Linear(3,128)
+        self.fc2=nn.Linear(128,64)
+        self.fc3=nn.Linear(64,1)
+
+
+    def forward(self,x):
+        x=F.relu(self.fc1(x))
+        x=F.relu(self.fc2(x))
+        mu=torch.tanh(self.fc3(x))*2
+
+        return mu
+
+class QNet(nn.Module):
+
+    def __init__(self,):
+        super(QNet,self).__init__()
+        self.fc_s=nn.Linear(3,64)
+        self.fc_a=nn.Linear(1,64)
+        self.fc_q=nn.Linear(128,32)
+        self.fc_out=nn.Linear(32,1)
+
+
+    def forward(self,s,a):
+
+        h1=F.relu(self.fc_s(s))
+        h2=F.relu(self.fc_a(a))
+        cat=torch.cat([h1,h2],dim=1)
+        q=F.relu(self.fc_q(cat))
+
+        return self.fc_out(q)
---
+++ Ensemble_DDPG/main4.py
@@ -0,0 +1,59 @@
+import gym
+import matplotlib.pyplot as plt
+import numpy as np
+import tqdm
+
+
+from SAMC7_C2 import SAMC7_C2
+from SAMC7_C4 import SAMC7_C4
+from SAMC8_C4 import SAMC8_C4
+from SAMC10_C4 import SAMC10_C4
+
+if __name__ == "__main__":
+    # environment
+    env_id = "Pendulum-v1"
+    env = gym.make(env_id)
+
+
+    num_frames=2000
+    memory_size=50000
+    batch_size=64
+    epoch=1
+    result_samc72=np.zeros([int(num_frames/10)],dtype=np.float32)
+    result_samc74 = np.zeros([int(num_frames / 10)], dtype=np.float32)
+    result_samc84 = np.zeros([int(num_frames / 10)], dtype=np.float32)
+    result_samc104 = np.zeros([int(num_frames / 10)], dtype=np.float32)
+
+
+    for i in tqdm.trange(epoch):
+        agent1=SAMC7_C2(env,memory_size,batch_size,tau=0.25)
+        agent2 = SAMC7_C4(env, memory_size, batch_size, tau=0.25)
+        agent3 = SAMC8_C4(env, memory_size, batch_size, tau=0.25)
+        agent4 = SAMC10_C4(env, memory_size, batch_size, tau=0.25)
+
+
+        # Reward_list
+        result1 = agent1.train(num_frames,a=0.4,b=0.6,w=0.01)
+        result2 = agent1.train(num_frames, a=0.4, b=0.6, w=0.01)
+        result3 = agent1.train(num_frames, a=0.4, b=0.6, w=0.01)
+        result4 = agent1.train(num_frames, a=0.4, b=0.6, w=0.01)
+        #result1 = agent1.train(num_frames)
+
+
+
+
+        result_samc72+=result1
+        result_samc74+=result2
+        result_samc84+=result3
+        result_samc104+=result4
+
+    #Visualization
+    plt.title('Ensemble main 4')
+    plt.xlabel('episode (X10)')
+    plt.ylabel('Averaged Reward')
+    plt.plot(result_samc72)
+    plt.plot(result_samc74)
+    plt.plot(result_samc84)
+    plt.plot(result_samc104)
+    plt.legend(['SAMC7_C2','SAMC7_C4','SAMC8_C4','SAMC10_C4'])
+    plt.show()
